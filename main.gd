ec010814dc743b6d183514104989152db64ba559
nile64 <87245996+Nile64@users.noreply.github.com>
2024-09-22 11:25:08 -0400

    add core stuff

---
+++ main.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@export var gradient : Gradient
+
+var colorStep = 0;
+var colorStepSpeed = 0.1;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Button/Window/Panel/ScrollContainer/VBoxContainer/VersionLabel.text = "   " + ProjectSettings.get_setting("application/config/version")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_change_bg_color(delta)
+
+func _change_bg_color(delta):
+	if colorStep >= 1:
+		colorStep = 0
+	
+	colorStep += delta * colorStepSpeed;
+	
+	RenderingServer.set_default_clear_color(gradient.sample(colorStep))
+
+func _open_credits_window():
+	$Button/Window.show();
